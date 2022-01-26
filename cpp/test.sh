87178226c490469c2f1aa2ced866e87537211e09
Quentin Quadrat <lecrapouille@gmail.com>
2022-01-26 21:10:56 +0100

    Only compile and run intersting C++ versions

--- cpp/test.sh
+++ cpp/test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
-echo "Compile threaded raw array"
-g++ --std=c++11 -W -Wall -Wextra -Wshadow-local idriss-thread-raw.cpp -o idriss-threaded-raw -lpthread
+#echo "Compile threaded raw array"
+#g++ --std=c++11 -W -Wall -Wextra -Wshadow-local idriss-thread-raw.cpp -o idriss-threaded-raw -lpthread
 
-echo "Compile threaded std::queue v0"
-g++ --std=c++11 -W -Wall -Wextra -Wshadow-local idriss-thread-queue-v0.cpp -o idriss-threaded-queue-v0 -lpthread
+#echo "Compile threaded std::queue v0"
+#g++ --std=c++11 -W -Wall -Wextra -Wshadow-local idriss-thread-queue-v0.cpp -o idriss-threaded-queue-v0 -lpthread
 
 echo "Compile threaded std::queue v1"
 g++ --std=c++11 -W -Wall -Wextra -Wshadow-local idriss-thread-queue-v1.cpp -o idriss-threaded-queue-v1 -lpthread
@@ -17,15 +17,15 @@ echo "================="
 echo "Run threaded std::queue v1"
 time ./idriss-threaded-queue-v1 $1 $2
 
-echo ""
-echo "================="
-echo "Run threaded raw array"
-time ./idriss-threaded-raw $1 $2
+#echo ""
+#echo "================="
+#echo "Run threaded raw array"
+#time ./idriss-threaded-raw $1 $2
 
-echo ""
-echo "================="
-echo "Run threaded std::queue v0"
-time ./idriss-threaded-queue-v0 $1 $2
+#echo ""
+#echo "================="
+#echo "Run threaded std::queue v0"
+#time ./idriss-threaded-queue-v0 $1 $2
 
 echo ""
 echo "================="
